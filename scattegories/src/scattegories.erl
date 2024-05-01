0e2f5ec0d64a499669cceb701d09dfa00deb1bd7
Jackson B. Warhover <jackson.warhover@tufts.edu>
2024-05-01 13:56:04 -0400

    fix printing

--- scattegories/src/scattegories.erl
+++ scattegories/src/scattegories.erl
@@ -8,13 +8,13 @@
 
 % start(UserName): starts the interpreter for the game
 start(UserName) ->
-    Mode = io:get_line("Would you like to create or join a room? 
-                                    [Enter: create or join]:  "),
+    Mode = io:get_line("Would you like to create or join a room? " ++
+                       "[Enter: create or join]:  "),
     case Mode of "create\n" ->
         io:format("Creating a game...~n", []),
         Room = io:get_line("Enter a RoomName:  "),
-        Rounds = io:get_line("Enter the number of rounds you would like to 
-                                                                    play: "),
+        Rounds = io:get_line("Enter the number of rounds you would like to " ++
+                             "play: "),
         [_H | Room1] = lists:reverse(Room),
         {Rounds1, _} = string:to_integer(Rounds),
         RoomName = lists:reverse(Room1),
@@ -28,8 +28,8 @@ start(UserName) ->
         p:join(UserName, PeerNode, nothing),
         start(UserName);
     _ ->
-        io:format("Invalid input. Please enter 'create' to create a room or 
-                                                    'join' to join a room.~n"),
+        io:format("Invalid input. Please enter 'create' to create a room or " ++
+                  "'join' to join a room.~n"),
             start(UserName)
     end.
 
