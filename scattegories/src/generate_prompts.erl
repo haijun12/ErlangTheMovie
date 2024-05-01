4a2d16dfc9c6dd538206a86d3054272bcae58983
ajalal03 <90590634+ajalal03@users.noreply.github.com>
2024-05-01 13:50:23 -0400

    added comments

--- scattegories/src/generate_prompts.erl
+++ scattegories/src/generate_prompts.erl
@@ -3,16 +3,19 @@
 %%% generate_prompts Module
 %%% 
 -module(generate_prompts).
--export([select_words/2, select_random_elements/2, select_letters/1,
-                                                    select_prompts/2]).
+-export([select_words/2, select_random_elements/2, select_letters/1,select_prompts/2]).
 
 -import(math, [pow/2]).
+
+%% select_prompts (FilePath, Number): Returns the categories and letters 
+%% for each game
 select_prompts (FilePath, Number) ->
     Words = select_words(FilePath, Number),
     Letters = select_letters(Number),
     Prompts = lists:zip(Letters, Words),
     Prompts.
-% Function to read a text file and select a random number of words
+%% select_words(FilePath, NumberOfCategories): Returns the categories for each 
+%% game
 select_words(FilePath, NumberOfCategories) ->
     {ok, File} = file:open(FilePath, [read]),
     Words = io:get_line(File, ''),
@@ -23,7 +26,8 @@ select_words(FilePath, NumberOfCategories) ->
     file:close(File),
     WordsToSelect.
 
-% Function to randomly select elements from a list
+%% select_random_elements(List, N): Returns a list of random elements
+%% given a list and number of elements to choose
 select_random_elements(_, 0) ->
     [];
 select_random_elements(List, N) when N > 0 ->
@@ -32,16 +36,15 @@ select_random_elements(List, N) when N > 0 ->
     Rest = lists:delete(Selected, List),
     [Selected | select_random_elements(Rest, N - 1)].
 
-% Function to randomly choose a number of different letters
-select_letters(NumberOfLetters) when is_integer(NumberOfLetters), 
-                                                    NumberOfLetters >= 0 ->
-    % Generate a list of ASCII values of uppercase letters
-    Alphabet = lists:seq($A, $Z),
-    % Select unique letters
-    UniqueLettersList = select_unique_elements(Alphabet, NumberOfLetters, []), 
+%% select_letters(NumberOfLetters): Returns a list of letters given the 
+%% number of letters to choose
+select_letters(NumberOfLetters) when is_integer(NumberOfLetters), NumberOfLetters >= 0 ->
+    Alphabet = lists:seq($A, $Z), % Generate a list of ASCII values for uppercase letters
+    UniqueLettersList = select_unique_elements(Alphabet, NumberOfLetters, []),
     lists:map(fun(X) -> [X] end, UniqueLettersList).
 
-% Function to select unique elements from a list
+%% select_unique_elements(List, N, Acc): Returns a list of unique elements 
+%% given a list and number of elements to vhoosr 
 select_unique_elements(_, 0, Acc) ->
     Acc;
 select_unique_elements(List, N, Acc) when N > 0 ->
